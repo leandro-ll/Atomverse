ca8435a8e67de9d97f102d56ee39051faeeb6c49
janualiv <byjanualiv_123>
2026-07-15 15:05:56 +0700

    fkin bugs:

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const API_CONFIG = {
     // Ganti dengan URL Railway kamu
-    BASE_URL: 'https://atomverse-backend-production.up.railway.app/',
+    BASE_URL: 'https://atomverse-backend-production.up.railway.app',
     
     CHAT: '/api/chat',
     INVERSE: '/api/inverse-experiment'
